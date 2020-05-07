06001b2d204622d2f5670e39b6823562f43c2c9f
Ralph - Collage Creative <ralphr@outlook.com>
2020-05-07 07:57:11 +0100

    courses dynamic list

--- application/routes.py
+++ application/routes.py
@@ -4,19 +4,21 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html", login=True)
+    return render_template("index.html", login=True, index = True)
 
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html", login=True)
+    courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+    print(courseData[0]["title"])
+    return render_template("courses.html", courseData = courseData, courses = True)
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=True)
+    return render_template("login.html", login=True, log = True)
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=True)
+    return render_template("register.html", login=True, register = True)
